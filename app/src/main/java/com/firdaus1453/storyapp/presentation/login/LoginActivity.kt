e7f6e39727fe6721afdfa40f57ee4f191ebb59f7
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-10-17 09:46:24 +0800

    Fix login

--- app/src/main/java/com/firdaus1453/storyapp/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/firdaus1453/storyapp/presentation/login/LoginActivity.kt
@@ -141,20 +141,6 @@ class LoginActivity : AppCompatActivity() {
                                                 true
                                             )
                                         )
-                                        AlertDialog.Builder(this).apply {
-                                            setTitle("Yeah!")
-                                            setMessage("Anda berhasil login.")
-                                            setPositiveButton("Lanjut") { _, _ ->
-                                                val intent =
-                                                    Intent(context, MainActivity::class.java)
-                                                intent.flags =
-                                                    Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                                                startActivity(intent)
-                                                finish()
-                                            }
-                                            create()
-                                            show()
-                                        }
                                     }
                                 }
                             }
