6a0d4f856b4ae882cea407ad2421b5243727c306
Muhammad Firdaus <mfirdaus1453@gmail.com>
2022-10-21 21:35:24 +0800

    Feature/check login (#6)
    * Implement startup activity
    
    * check login in StartupActivity

--- app/src/main/java/com/firdaus1453/storyapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/firdaus1453/storyapp/presentation/main/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.firdaus1453.storyapp.presentation.main
 
+import android.content.Intent
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.firdaus1453.storyapp.R
 import com.firdaus1453.storyapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.firdaus1453.storyapp.presentation.createstory.CreateStoryActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +20,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.bottomNavMain
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.fabAddStory.setOnClickListener {
+            val intent = Intent(this, CreateStoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
